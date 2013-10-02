4409c9aca755c17576aa541f4d830e4fa1ce58ef
Hobin Yoon <hobinyoon@gmail.com>
2013-10-02 13:30:41 -0400

    clean up

--- misc/rsync-twitter-data.sh
+++ misc/rsync-twitter-data.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 while [ 1 ]; do
+	echo "mdc-s60"
 	time rsync -av --delete /mnt/multidc-data/twitter mdc-s60:/mnt/multidc-data
+	echo
+	echo "mdc-s80"
 	time rsync -av --delete /mnt/multidc-data/twitter mdc-s80:/mnt/multidc-data
+	echo
+	echo "mdc-p40"
 	time rsync -av --delete /mnt/multidc-data/twitter mdc-p40:/mnt/multidc-data
-	date
-	sleep $((2*3600))
+	echo
+	SLEEP_SEC=$((2*3600))
+	echo `date`" Sleeping for "$SLEEP_SEC" seconds ..."
+	sleep $SLEEP_SEC
+	echo
 done
